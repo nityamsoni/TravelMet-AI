f11750bb50d9acff9c687b2ec645ef3433beb304
Nityam <nityamsoni26@gmail.com>
2026-09-18 18:51:04 +0530

    addd fast api sever and all

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from tools.tavily_tool import tavily_search
-from tools.flight_ttol import search_flights
-
+from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
 
 # res= tavily_search("Give me best 5 hotels in India")
@@ -9,5 +9,18 @@ from tools.flight_ttol import search_flights
 
 
 
-res= search_flights("Plan 7 days trip IND  to Paris")
-print(res)
+# res= search_flights("Plan 7 days trip IND  to Paris")
+# print(res)
+
+
+
+
+user_input = input("Enter travel request: ")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test_user"
+)
+
+print("\nFINAL RESPONSE:\n")
+print(response["answer"])
